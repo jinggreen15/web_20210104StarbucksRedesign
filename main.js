68d3f3203ad893c690cc3708286f132b5793ad98
jinggreen15 <jinggreen15@gmail.com>
2021-01-13 21:42:23 +0800

    0113

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-$("banner").silck();
+$("#banner").slick({
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    dots: true
+});
+
+$("#service").slick({
+    // 一頁顯示三格
+    slidesToShow: 3
+});
